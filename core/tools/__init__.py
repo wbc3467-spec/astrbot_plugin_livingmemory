a2ba4791069d45f52f818b9a7af87c626ec8b1e9
wbc3467-spec <wbc3467@gmail.com>
2026-06-11 06:32:50 +0000

    Added memory delete tool

--- core/tools/__init__.py
+++ core/tools/__init__.py
@@ -2,5 +2,6 @@
 
 from .memory_memorize_tool import MemoryMemorizeTool
 from .memory_search_tool import MemorySearchTool
+from .memory_delete_tool import MemoryDeleteTool
 
-__all__ = ["MemoryMemorizeTool", "MemorySearchTool"]
+__all__ = ["MemoryMemorizeTool", "MemorySearchTool", "MemoryDeleteTool"]
